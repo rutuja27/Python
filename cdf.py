f6bcb0254272e2982eca972b23267996e66a53db
Rutuja <27rutuja@gmail.com>
2020-12-23 15:49:30 -0500

    Change in max of two camera code

--- cdf.py
+++ cdf.py
@@ -11,22 +11,6 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 
-def getMaxSpike(filename, max_arr, spike_threshold, inter_latency):
-    
-    
-    data_grab = csv.reader(filename, delimiter=',')
-    prev=0;   
-    for idx,row in enumerate(data_grab):
-        
-        if(inter_latency): 
-            if(idx == 0):
-              prev =  np.float(row[1])/1000
-            else:
-              if(((np.float(row[1])/1000) - prev) >= spike_threshold and idx <= 99999):
-                  max_arr[idx] = max(max_arr[idx], (np.float(row[1])/1000) - prev)
-              prev =  (np.float(row[1])/1000)   
-    
-
 
 def readcsvFile(filename, arr, max_arr, spike_threshold, inter_latency):
     
@@ -46,13 +30,14 @@ def readcsvFile(filename, arr, max_arr, spike_threshold, inter_latency):
               prev =  (np.float(row[1])/1000)  
             
 
-def getMaxCam(cdf_list, arr, no_cam):
+def getMaxCam(max_cam, cam0, cam1):
     
-    for i in range(1, no_cam):
-        for j in range(0, len(cdf_list[i])):
-            arr[j] = max(cdf_list[i][j], arr[j])
+    max_spikes = [] 
+    for i in range(0, len(cam0)):
+        max_cam[i] = max(cam0[i], cam1[i])
             
-    
+    max_list = [val for idx,val in enumerate(max_cam) if val != 0.0]
+    return max_list;
 
 
 def plot_histogram(hist_list, bins, binsize, bin_shft,  norm_factor, 
@@ -88,11 +73,13 @@ def main():
     
     ## read csv files
     #'C:/Users/27rut/BIAS/misc/imagegrab_day_trials/two_camera/', 
-    dir_list = ['C:/Users/27rut/BIAS/misc/signal_slot_day_trials/two_camera/'];
-    days = ['10_12_2020/', '14_12_2020/', '15_12_2020/']
+    dir_list = ['C:/Users/27rut/BIAS/misc/signal_slot_day_trials/two_camera/', 'C:/Users/27rut/BIAS/misc/jaaba_plugin_day_trials/two_camera/'];
+    days = ['21_12_2020/', '22_12_2020/', '23_12_2020/']
+    file_names = ['signal_slot_f2f', 'jaaba_f2f']
     cam = ['0','1']
     cdf_hist_list = [];
-    dir_len = len(cam) + 1
+    max_camera_hist_list = []
+    dir_len = len(dir_list)
     
      ## experiment variables
     inter_latency=1;
@@ -103,34 +90,57 @@ def main():
     no_of_trials = 5
     framerate = 500
     no_of_days = len(days)
+    no_of_cam = 1 #len(cam)
     norm_factor= float(framerate/(no_of_frames*no_of_trials*no_of_days))
-    print(norm_factor)
+    print("Norm factor: " , norm_factor)
     
-    max_lat = np.zeros(no_of_frames,dtype=float)
+    cam0_list = []
+    cam1_list = []
+    #max_lat = np.zeros(no_of_frames,dtype=float)
+    max_cam = 0
+    max_lat = np.zeros(no_of_frames, dtype=float)
+    max_cam0 = np.zeros(no_of_frames, dtype=float)
+    max_cam1 = np.zeros(no_of_frames, dtype=float)
     
-    
-    for frame_dir in dir_list:
-        #data_arr = []
-        for cam in cam:
-            data_arr=[]    
-            for day in days:
-                #data_arr=[] 
-                for i in range(1,no_of_trials+1):
-                    #data_arr = []               
-                    try:
-                        file_handle = open(frame_dir + day + 'signal_slot_f2f' + cam + '_trial' + str(i)  + '.csv', 'r+');
-                    except IOError:
-                        print (' File could not be open. Check file location')
-                        return -1;
-                    readcsvFile(file_handle, data_arr,max_lat, min_spike_threshold,inter_latency);                  
-            cdf_hist_list.append(data_arr)    
+    for idx, frame_dir in enumerate(dir_list):
+        cam0_list = []
+        cam1_list = []
+       
+        for day in days:
             
+            for i in range(1,no_of_trials+1):
+                if(max_cam):
+                    max_cam0.fill(0);
+                    max_cam1.fill(0);
+                    max_lat.fill(0);
+                try:
+                    if(no_of_cam==2):
+                        cam0_handle = open(frame_dir + day + file_names[idx] + cam[0] + '_trial' + str(i)  + '.csv', 'r+')
+                        cam1_handle = open(frame_dir + day + file_names[idx] + cam[1] + '_trial' + str(i)  + '.csv', 'r+')
+                        readcsvFile(cam0_handle, cam0_list, max_cam0, min_spike_threshold, inter_latency);  
+                        readcsvFile(cam1_handle, cam1_list, max_cam1, min_spike_threshold, inter_latency); 
+                        if(max_cam):
+                           max_camera_hist_list.append(getMaxCam(max_lat, max_cam0, max_cam1))
+                    else:
+                        cam0_handle = open(frame_dir + day + file_names[idx] + cam[0] + '_trial' + str(i)  + '.csv', 'r+')     
+                        readcsvFile(cam0_handle, cam0_list, max_cam0, min_spike_threshold, inter_latency);  
+                        
+                except IOError:
+                    print (' File could not be open. Check file location')
+                    return -1;
+                                
+        if(no_of_cam==2):
+            cdf_hist_list.append(cam0_list)
+            cdf_hist_list.append(cam1_list)
+        else:
+            cdf_hist_list.append(cam0_list)
+            
+    #flatten the list  and append 
+    if(no_of_cam == 2 and max_cam):
+        max_camera_hist_list = [y for x in max_camera_hist_list for y in x]
+        cdf_hist_list.append(max_camera_hist_list)
     
-    #plot max of two cameras 
-    #plt.plot(max_lat)
-    cdf_hist_list.append(max_lat)
-    
-    
+    #print(cdf_hist_list)
     ## plot hists and cdf
     fig, axes = plt.subplots(nrows=2, ncols=1)
     fig.tight_layout(pad=3.0)
@@ -179,10 +189,10 @@ def main():
     ax1.set_ylabel('spikes/secs',fontsize=8)
     ax1.set_xlabel('Latency of Spikes',fontsize=8)
     
-    labels = ['Cam 1', 'Cam 2', 'Max']
+    labels = ['Signal Slot Cam0', 'Signal Slot Cam1', 'Max']
     ax0.legend(labels, fontsize=7)
     
-    fig.savefig('C:/Users/27rut/BIAS/misc/signal_slot_day_trials/comp_plugin_maxtwocameras.pdf')
+    #fig.savefig('C:/Users/27rut/BIAS/misc/signal_slot_day_trials/comp_imagegrabw&woplugin_twocameras.png')
                 
                 
     
--- plot_time.py
+++ plot_time.py
@@ -14,55 +14,88 @@ import matplotlib
 import matplotlib.pyplot as plt
 
  
-def readcsvFile(filename, arr):
+def readcsvFile(filename, arr, inter_latency):
+  
     data_grab = csv.reader(filename, delimiter=',')
+    prev=0;   
     for idx,row in enumerate(data_grab):
-        arr[idx] = np.float(row[1])/1000
+        if(not inter_latency):
+            arr.append(np.float(row[1])/1000)
+        else: 
+            if(idx == 0):
+              prev =  np.float(row[1])/1000
+            else:
+                arr.append((np.float(row[1])/1000) - prev)
+            prev =  (np.float(row[1])/1000)      
         
 
-def plot_data(arr, inter_latency):
+def plot_data(arr, inter_latency, shape, color):
     
-    sz = arr.size;
+    sz = len(arr)
     if inter_latency:
-        lat_val=np.diff(arr)
-        plt.plot(lat_val[0:sz-2],'.')
+        plt.plot(arr[0:sz-2],'.',color=color, marker=shape)
     else:
-        plt.plot(arr[0:],'.')
+        plt.plot(arr[0:],'.',color=color, marker=shape)
    
 
 def main():
 
-    inter_latency = 1;
+    inter_latency = 1
     no_of_frames = 100000
+    cam = ['0', '1']
+    no_of_cam = 1
     
-    dir_list =['C:/Users/27rut/BIAS/misc/signal_slot_day_trials/two_camera/10_12_2020/']
-               #'C:/Users/27rut/BIAS/misc/imagegrab_day_trials/7_12_2020/',
-               #'C:/Users/27rut/BIAS/misc/imagegrab_day_trials/6_12_2020/']
-    lat_val = np.zeros((no_of_frames),dtype = np.float)
-
+    dir_list =['C:/Users/27rut/BIAS/misc/jaaba_plugin_day_trials/two_camera/21_12_2020/',
+               'C:/Users/27rut/BIAS/misc/jaaba_plugin_day_trials/two_camera/22_12_2020/',
+               'C:/Users/27rut/BIAS/misc/jaaba_plugin_day_trials/two_camera/23_12_2020/']
+   
+    cam0_list = []
+    cam1_list = []
 
     fig = plt.figure();
+    shapes= ['o', 'x', '^', 'P', '*']
+    color = ['r', 'b']
 
     for frame_dir in dir_list:
-     
-       for i in range(1,4):
-            
-           lat_val.fill(0.0)
+       
+       for i in range(1,6):
+           
+           cam0_list = []
+           cam1_list = []
            try:
-               file_handle = open(frame_dir + 'signal_slot_f2f1_trial'+ str(i)  + '.csv', 'r+');
+               if(no_of_cam == 2):
+                   cam0_handle = open(frame_dir + 'jaaba_f2f' + cam[0] + '_trial'+ str(i)  + '.csv', 'r+');
+                   cam1_handle = open(frame_dir + 'jaaba_f2f' + cam[1] + '_trial'+ str(i)  + '.csv', 'r+');
+               else:
+                   cam0_handle = open(frame_dir + 'jaaba_f2f' + cam[0] + '_trial'+ str(i)  + '.csv', 'r+');
+                   
+            
            except IOError:
                print (' File could not be open. Check file location',i)
                return -1;
+           if(no_of_cam==2):
+               readcsvFile(cam0_handle, cam0_list, inter_latency)
+               readcsvFile(cam1_handle, cam1_list, inter_latency)
+               plot_data(cam0_list, inter_latency, shapes[i-1], color[0])
+               plot_data(cam1_list, inter_latency, shapes[i-1], color[1])
+           else:
+               readcsvFile(cam0_handle, cam0_list, inter_latency)
+               plot_data(cam0_list, inter_latency, shapes[i-1], color[0])
+              
           
-           readcsvFile(file_handle, lat_val)
-           plot_data(lat_val, inter_latency)
-    file_handle.close 
+           
+        
     plt.plot(5*np.ones((no_of_frames)))
     plt.xlabel('Frames')
     plt.ylabel('Milliseconds')
-    plt.title('ImageGrab f2f Latency')
+    plt.title('Jaaba Plugin - 2 Cameras')
+    labels = ['Cam 0']
+    plt.legend(labels, fontsize=7)
     plt.show()     
-    #fig.savefig('C:/Users/27rut/BIAS/misc/signal_slot_day_trials/figs/imagegrab_f2f_latencies.pdf')
+    #
+    
+    
+    #fig.savefig('C:/Users/27rut/BIAS/misc/signal_slot_day_trials/figs/_twocameras.png')
 
 
 if __name__ == "__main__":
